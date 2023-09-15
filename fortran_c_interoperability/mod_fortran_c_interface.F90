447497e4206fc0cc4c78089dfde4b24168556df2
Maruthi N. H <maruthinh@gmail.com>
2023-09-15 21:34:02 +0530

    Deleted unwanted portion of the code from mod_fortran_c_interface

--- fortran_c_interoperability/mod_fortran_c_interface.F90
+++ fortran_c_interoperability/mod_fortran_c_interface.F90
@@ -21,36 +21,3 @@ module fortran_c_interface
     end subroutine sum_two_arrays 
   end interface 
 end module fortran_c_interface 
-
-!program test_fortran_c_interoperability
-!  use, intrinsic :: iso_c_binding 
-!  use c_interface_func
-!  implicit none 
-!  
-!  real (c_double), allocatable, target, dimension(:) :: a, b
-!  real (c_double), pointer :: c(:)
-!  real (c_double) :: sum  
-!  integer (c_int) :: nx, i  
-!
-!  nx=5
-!  allocate(a(0:nx-1))
-!  allocate(b(0:nx-1))
-!  allocate(c(0:nx-1))
-!  
-!  do i=0,nx-1
-!    a(i)=i
-!    b(i) = i+1
-!  enddo
-!
-!  sum = array_sum(c_loc(a), nx)
-!  print*, "Sum of array computed from C function (called from Fortran): ", sum 
-!
-!  call sum_two_arrays(nx, c_loc(a), c_loc(b), c)
-!
-!  print*, "Summed array is: ", c
-!  
-!  deallocate(a,b,c)
-!
-!end program test_fortran_c_interoperability 
-
-
